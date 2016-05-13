f21205e7cdc692b2cfc7869f3645a22b7fe19c6e
Daniele Montesi <daniele.montesi@gmail.com>
2016-05-13 12:33:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
 <?php
 
-echo "Hello";
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
+class DataslangApi {
+  
+  public function __construct(){
+    $this->client = new Client([
+        // Base URI is used with relative requests
+        'base_uri' => 'http://api.dataslang.com',
+        // You can set any number of default request options.
+        'timeout'  => 2.0,
+    ]);
+}
+
+  public function validate($xml, $xsd){
+    $response = $this->client->request('POST', '/validate', [
+        'form_params' => [
+            'field_name' => 'abc',
+            'other_field' => '123',
+            'nested_field' => [
+                'nested' => 'hello'
+            ]
+        ]
+    ]);
+  
+  }
+
+}
