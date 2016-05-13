96cc1520e7db1bd767e88a99b1faf0787201f5e6
Daniele Montesi <daniele.montesi@gmail.com>
2016-05-13 12:35:50 +0200

    Create DataslangApiTest.php

---
+++ tests/DataslangApiTest.php
@@ -0,0 +1,16 @@
+<?php
+class DataslangApiTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testValidate()
+    {
+        $api = new DataslangApi();
+
+        // $api->validate()
+
+        // Assert
+        // $this->assertEquals...
+    }
+
+    // ...
+}
