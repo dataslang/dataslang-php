9f8202ec588a9e20697b916babaafd8f08f84724
danielemontesi <daniele.montesi@gmail.com>
2016-05-16 11:30:29 +0200

    Renamed DataslangApi

---
+++ DataslangApi.php
@@ -0,0 +1,68 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
+class DataslangApi {
+	
+	const BASE_URI = 'http://api.dataslang.com';
+	
+	private $client = null;
+	private $settings = array();
+	private $timeout = 2.0;
+  
+	public function __construct(){
+		$this->initSettings();		
+    	$this->client = new Client($this->settings);
+	}
+
+	public function validate($xml, $xsd){
+		$result = null;
+		
+		try {
+		
+
+			//     	$response = $this->client->request('POST', '/validate', [
+			//         	'form_params' => [
+			//             	'field_name' => 'abc',
+			//             	'other_field' => '123',
+			//             	'nested_field' => [
+			//                 	'nested' => 'hello'
+			//             	]
+			//         	]
+			//     	]);
+			
+			$response = $this->client->request('POST', '/validate', [
+				'form_params' => [
+					'parametro' => 'prova'
+				]
+			]);
+			
+			$result = $res->getBody();
+			
+		} catch (\Exception $e) {
+			
+		}
+		
+		return $result;
+  	}
+  
+  	private function initSettings(){
+  		$this->settings['base_uri'] = $this->getBaseUri();
+  		$this->settings['timeout'] = $this->getTimeout();
+  	}
+  
+	public function setTimeout($timeout){
+		$this->timeout = $timeout;
+	}
+	
+	public function getTimeout(){
+		return $this->timeout;
+	}
+	
+	public function getBaseUri(){
+		return self::BASE_URI;
+	}
+
+}
