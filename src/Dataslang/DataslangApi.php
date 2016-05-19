a2044664763e99930422439b1e6a701046007ccd
danielemontesi <daniele.montesi@gmail.com>
2016-05-19 10:44:11 +0200

    Updated DataslangApi.php

--- src/Dataslang/DataslangApi.php
+++ src/Dataslang/DataslangApi.php
@@ -6,50 +6,76 @@ use GuzzleHttp\Client;
 
 class DataslangApi {
 	
-	const BASE_URI = 'http://api.dataslang.com';
+	const BASE_URI = 'http://api.dataslang';
 	
 	private $client = null;
 	private $settings = array();
-	private $timeout = 2.0;
-  
-	public function __construct(){
-		$this->initSettings();
-		$this->client = new Client($this->settings);
-	}
+	private $timeout = 7;
 
 	public function validate($xml, $xsd){
 		$result = null;
-		
-		try {
 
-			//     	$response = $this->client->request('POST', '/validate', [
-			//         	'form_params' => [
-			//             	'field_name' => 'abc',
-			//             	'other_field' => '123',
-			//             	'nested_field' => [
-			//                 	'nested' => 'hello'
-			//             	]
-			//         	]
-			//     	]);
+		try {
 			
-			$response = $this->client->request('POST', '/validate', [
+			$client = $this->getClient();
+			$response = $client->request('POST', '/xml/validate', [
 				'form_params' => [
-					'parametro' => 'prova'
+					'xml' => urlencode($xml),
+					'xsd' => urlencode($xsd)
 				]
 			]);
 			
-			$result = $res->getBody();
+			$result = $response->getBody();
 			
 		} catch (\Exception $e) {
-			
+
 		}
 		
 		return $result;
   	}
-  
-  	private function initSettings(){
-  		$this->settings['base_uri'] = $this->getBaseUri();
-  		$this->settings['timeout'] = $this->getTimeout();
+  	
+  	public function transformToXml($xml){
+  		$result = null;
+  		
+  		try {
+  				
+  			$client = $this->getClient();
+  			$response = $client->request('POST', '/xml/transform/xml', [
+  				'form_params' => [
+  					'xml' => urlencode($xml)
+  				]
+  			]);
+  				
+  			$result = $response->getBody();
+  				
+  		} catch (\Exception $e) {
+
+  		}
+  		
+  		return $result;
+  	}
+  	
+  	public function transformToHtml($xml, $xsl, $dest_path){
+  		$result = null;
+  		
+  		try {
+  		
+  			$client = $this->getClient();
+  			$response = $client->request('POST', '/xml/transform/html', [
+				'form_params' => [
+					'xml' => urlencode($xml),
+					'xsl' => urlencode($xsl),
+					'dest_path' => urlencode($dest_path)
+				]
+  			]);
+  		
+  			$result = $response->getBody();
+  		
+  		} catch (\Exception $e) {
+
+  		}
+  		
+  		return $result;
   	}
   
 	public function setTimeout($timeout){
@@ -62,6 +88,14 @@ class DataslangApi {
 	
 	public function getBaseUri(){
 		return self::BASE_URI;
+	}
+	
+	private function getClient(){
+		$settings = array();
+		$settings['base_uri'] = $this->getBaseUri();
+		$settings['timeout'] = $this->getTimeout();
+		
+		return new Client($settings);
 	}
 
 }
